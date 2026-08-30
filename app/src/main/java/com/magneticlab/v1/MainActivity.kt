c783628e756cca5a9fc793dca7688a2bae0c81d6
𝐚𝐥𝐩𝐡𝐚 <alpha470rx@gmail.com>
2026-08-30 17:41:26 +0700

    Update MainActivity.kt

--- app/src/main/java/com/magneticlab/v1/MainActivity.kt
+++ app/src/main/java/com/magneticlab/v1/MainActivity.kt
@@ -12,7 +12,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -102,7 +103,10 @@ private fun magneticPoint(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(
+    ExperimentalMaterial3Api::class,
+    ExperimentalComposeUiApi::class
+)
 @Composable
 private fun MagneticLabApp() {
 
